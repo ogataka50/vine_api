c8e053d98e00474ba2e51bbc5d7c8ab3d32e97c4
ogataka50 <ogataka50@gmail.com>
2018-01-21 00:23:06 +0900

    add phan

---
+++ src/hoge.php
@@ -0,0 +1,15 @@
+<?php
+
+class Hoge
+{
+
+    /**
+     * exec_like_post description
+     * @param  array  $array
+     * @return int
+     */
+    public function exec_like_post($array = [])
+    {
+        return 111;
+    }
+}
