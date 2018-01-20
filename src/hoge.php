a63963a2b2e3ddfebc3217380a486960166af783
ogataka50 <ogataka50@gmail.com>
2018-01-21 01:18:34 +0900

    fix

--- src/hoge.php
+++ src/hoge.php
@@ -1,14 +1,15 @@
 <?php
 
+namespace Vine;
+
 class Hoge
 {
-
     /**
      * exec_like_post description
      * @param  array  $array
      * @return int
      */
-    public function exec_like_post($array = [])
+    public function execPost($array = [])
     {
         return 111;
     }
